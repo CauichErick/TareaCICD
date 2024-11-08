4cb027afed07e40af78c6e76405709595dfc78e8
HectorRuizMX <hector.ruiz@upnify.com>
2024-11-07 20:02:24 -0500

    Se crea funcion para agregar un boton

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ function mostrarAlerta() {
     alert("Hola Mundo");
 }
 
+function crearBoton() {
+    const contenedor = document.querySelector('#contenedor');
+
+    if(!contenedor) {
+        console.error('No existe el contenedor');
+        return;
+    }
+
+    const boton = document.createElement('button');
+    boton.innerText = 'Click';
+    boton.id = 'btn-click';
+    boton.textContent = 'Click';
+    contenedor.appendChild(boton);
+}
+
 module.exports = {
-    mostrarAlerta
+    mostrarAlerta,
+    crearBoton
 };
--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,25 @@
  * @jest-environment jsdom
 */
 
-const { mostrarAlerta } = require('../index');
+const { mostrarAlerta, crearBoton } = require('../index');
 
 describe('Pruebas en index.js', () => {
     beforeAll(() => {
         jest.spyOn(window, 'alert').mockImplementation(() => { });
+        jest.spyOn(console, 'error').mockImplementation(() => { });
     });
     test('debe mostrar un alerta', () => {
         expect(window.alert).not.toHaveBeenCalled();
         mostrarAlerta();
         expect(window.alert).toHaveBeenCalledWith('Hola Mundo');
+    });
+    test('debe crear un botón', () => {
+        crearBoton();
+        expect(console.error).toHaveBeenCalled();
+        expect(document.querySelector('#contenedor')).toBeNull();
+
+        document.body.innerHTML = '<div id="contenedor"></div>';
+        crearBoton();
+        expect(document.querySelector('#contenedor').innerHTML).toContain('<button id="btn-click"></button');
     });
 });
